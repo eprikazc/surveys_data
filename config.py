7a9dea8eddba41e324ed387a6876e9231c85be9c
Eugene Prikazchikov <eprikazc@gmail.com>
2017-05-31 10:20:33 +0300

    Read config from project dir, not from current dir

--- config.py
+++ config.py
@@ -1,4 +1,7 @@
 import configparser
+import os
 
+project_dir = os.path.dirname(os.path.abspath(__file__))
+config_path = os.path.join(project_dir, 'config.ini')
 config = configparser.ConfigParser()
-config.read('config.ini')
+config.read(config_path)
